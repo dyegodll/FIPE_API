dd55fa1eb19d1c00e97922b90b6676d778304810
Dyego Lima <dyegodll18@gmail.com>
2024-11-03 10:36:38 -0300

    first commit

---
+++ backend/src/main/java/project/unimed/dev/fipe_api/FipeApiApplication.java
@@ -0,0 +1,13 @@
+package project.unimed.dev.fipe_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/project/unimed/dev/fipe_api/FipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.unimed.dev.fipe_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
